8ee5eac53d44fc099a0c0c098775b734e467a0a8
aromarious <6535448+aromarious@users.noreply.github.com>
2024-12-14 17:07:40 +0900

    1.0.1

---
+++ release-update.js
@@ -0,0 +1,8 @@
+import fs from "fs"
+import Mustache from "mustache"
+const args = process.argv.slice(2)
+const [inputFilename, outputFilename, viewJSON] = args
+const input = fs.readFileSync(inputFilename, "utf8")
+const view = JSON.parse(viewJSON)
+const output = Mustache.render(input, view)
+fs.writeFileSync(outputFilename, output)
---
+++ release-update.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+node release-update.js clear-notifications.tmpl clear-notifications '{"version": "'$(git tag)'"}'
+chmod +x clear-notifications
+git add .
